c25f16b3c9817a054c5c5c0271d5035b5f41441d
hsaka-for <freto2022@gmail.com>
2026-07-29 11:39:47 +0200

    wip

--- src/ui.js
+++ src/ui.js
@@ -9,9 +9,18 @@ export function draw(
 ) {
   const canvas = document.getElementById("canvas")
   const ctx = canvas.getContext("2d")
+  let feldGröße = 50
 
+  for (let zeile = 0; zeile < 8; zeile++) {
+    for (let spalte = 0; spalte < 8; spalte++) {
+      let x = spalte * feldGröße
+      let y = zeile * feldGröße
+      ctx.strokeRect(x, y, feldGröße, feldGröße)
+      /*onClick(x, y, feldGröße, feldGröße, () => {
+        console.log("angeklickt")
+        state.G.moves.geldNehmen()
+      })*/
+    }
+  }
   // draw here
 }
-
-array = [1, 2, 3, 4]
-array.splice()
