e55f16fea2d56bea47c6f46d2f1cb0c2db048214
hsaka-for <freto2022@gmail.com>
2026-07-30 09:44:19 +0200

    wip

--- src/ui.js
+++ src/ui.js
@@ -25,10 +25,14 @@ export function draw(
     }
   }
   // draw here
+  ctx.fillStyle = "black"
   ctx.fillRect(425, 0, 200, 75)
   ctx.fillStyle = "white"
   ctx.font = "20px Arial"
   ctx.textAlign = "center"
   ctx.textBaseline = "middle"
   ctx.fillText("Geld nehmen", 525, 38)
+  onClick(425, 0, 200, 75, () => {
+    moves.geldNehmen()
+  })
 }
