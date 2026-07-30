5b28b1f6485e10faee93b22584ef8d7a9a69ac2f
hsaka-for <freto2022@gmail.com>
2026-07-30 10:09:33 +0200

    wip

--- src/ui.js
+++ src/ui.js
@@ -17,16 +17,17 @@ export function draw(
     for (let spalte = 0; spalte < 8; spalte++) {
       let x = spalte * feldGröße
       let y = zeile * feldGröße
-      console.log(state.G.marktplatz)
       for (const karte of state.G.marktplatz) {
         if (x == karte[0] && y == karte[1]) {
-          ctx.strokeStyle = "orangered"
+          ctx.strokeStyle = "orange"
+          console.log("BurgerPommes auf die 1")
+          ctx.strokeRect(x - 5, y - 5, feldGröße - 10, feldGröße - 10)
         } else {
           ctx.strokeStyle = "black"
+          ctx.strokeRect(x, y, feldGröße, feldGröße)
         }
       }
 
-      ctx.strokeRect(x, y, feldGröße, feldGröße)
       onClick(x, y, feldGröße, feldGröße, () => {
         console.log("angeklickt")
         // moves.geldNehmen() // nachdem Felder markiert wurden, Sachen vom Marktplatz kaufen
