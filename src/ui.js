e5b513d1295def60f4680f2f18e45790cce5da23
hsaka-for <freto2022@gmail.com>
2026-07-31 10:12:52 +0200

    wip

--- src/ui.js
+++ src/ui.js
@@ -51,6 +51,7 @@ export function draw(
           onClick(x, y, feldGröße, feldGröße, () => {
             console.log("angeklickt")
             moves.grundstückKaufen(i)
+            draw(state, moves)
             for (let zeile = 0; zeile < 8; zeile++) {
               for (let spalte = 0; spalte < 8; spalte++) {
                 if (state.G.spielfeld[spalte][zeile] == "0") {
@@ -73,7 +74,6 @@ export function draw(
             }
             ctx.fillStyle = "white"
             ctx.fillRect(15, 465, 370, 70)
-            draw(state)
           })
         }
       }
