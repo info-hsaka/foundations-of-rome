0c259823a8d1d17d90eec73714277ae4b7fa390d
hsaka-for <freto2022@gmail.com>
2026-07-31 12:29:41 +0200

    wip

--- src/ui.js
+++ src/ui.js
@@ -64,8 +64,7 @@ export function draw(
   const canvas = document.getElementById("canvas")
   const ctx = canvas.getContext("2d")
   let feldGröße = 50
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  ctx.fillStyle = "white"
+  //ctx.clearRect(0, 0, canvas.width, canvas.height)
   marktplatzMalen(state)
 
   for (let zeile = 0; zeile < 8; zeile++) {
