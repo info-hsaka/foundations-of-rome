d10c423494e21616ddf4591c589445464e4ca19a
hsaka-for <freto2022@gmail.com>
2026-08-06 10:08:31 +0200

    wip

--- src/Game.js
+++ src/Game.js
@@ -49,7 +49,7 @@ export const Game = {
             gebaut: false,
             wert: 1,
             id: 0,
-            grafik: "src\assets\rot\geb-0.png",
+            grafik: "rot\geb-0.png",
           },
           {
             koordinaten: [1040, 240],
